99b8f72c0c518340b1c1b126f27ed4832a2cb91d
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-09-14 04:20:51 +0000

    2

--- task_02.py
+++ task_02.py
@@ -4,5 +4,8 @@
 
 
 import inquisition
-FLEMISH = inquisition.SPANISH.replace('Spanish', 'Flemish')
+DL = 'Spanish'
+print len(DL)
+print inquisition.SPANISH.index('Spanish')
+FLEMISH = inquisition.SPANISH[0:19] + 'Flemish' + inquisition.SPANISH[26:]
 print FLEMISH
